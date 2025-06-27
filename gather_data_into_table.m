7cafd227b92bd1a79dcf8ab881c3dadbfd7578ab
David (secondary Dell) <davidswygart@gmail.com>
2025-06-27 00:00:33 -0400

    Create gather_data_into_table.m

---
+++ gather_data_into_table.m
@@ -0,0 +1,53 @@
+clear
+folders = dir();
+folders = folders(3:end, :); % get rid of . and ..
+folders = folders([folders.isdir]); % only include directories
+
+exp_table = table();
+for ind = 1:height(folders)
+    day_table = load_exp_and_licks(folders(ind).name);
+    exp_table = cat(1, exp_table, day_table);
+end
+
+save('all_days.mat',"exp_table")
+
+function exp = load_exp_and_licks(folder)
+f = [folder, filesep];
+exp = readtable([f, 'experiment_structure.csv']);
+
+licks_file = dir([f, 'licks_*.csv']).name;
+licks = readtable([f, licks_file]);
+exp.licks_L = get_licks_for_sippers(exp.sipper_L, licks);
+exp.licks_R = get_licks_for_sippers(exp.sipper_R, licks);
+exp.day(:) = string(folder);
+end
+
+function sorted_licks = get_licks_for_sippers(sippers, licks)
+sorted_licks = cell(size(sippers));
+for ind = 1:length(sippers)
+    is_sipper = sippers(ind) == licks.sipper_id;
+    is_start = logical(licks.state);
+    start = licks.timestamp(is_sipper & is_start);
+    stop = licks.timestamp(is_sipper & ~is_start);
+    sanity_check_lick_times(start,stop, sippers(ind))
+    sorted_licks{ind} = table(start, stop);
+end
+end
+
+function sanity_check_lick_times(start,stop, sipper)
+    sipper = string(sipper);
+    if length(start) ~= length(stop)
+         error("unequal number of lick starts and stops for sipper " + sipper)
+    end
+
+    duration = stop - start;
+    if any(duration<0)
+        warning("negative lick duration computer for sipper " + sipper)
+    end
+
+    max_d = max(duration);
+    if max_d>1000
+        d = string(max_d);
+        warning("suspiciously long lick duration ("+d+") detected for " + sipper)
+    end
+end
