c2d224c4c2b7e50df6b396380a4bad257ae729c1
David (secondary Dell) <davidswygart@gmail.com>
2025-06-26 22:36:23 -0400

    generate dummy licks from dummy consumption

--- generate_dummy_data.m
+++ generate_dummy_data.m
@@ -1,38 +1,23 @@
 clear
 %% settings
 time_stop = 1000 * 60 * 60; % 1 hour max
-
-lick_rate = 1; % Hz
-
-ethanol_prop_baseline = 0.8; % proportion of time drinking ethanol
-wistar_reduction = 0.7; % wistar drink less
-male_reduction = 0.8; % males drink less
-
 average_lick_duration = 100 ; % milliseconds
+licks_per_ml = 1000 / 6;
 %% load experiment_structure.csv
 exp_table = readtable('experiment_structure.csv');
 timestamps = [];
 sipper = [];
 
+total_consumed = exp_table.consumed_L + exp_table.consumed_R;
+r_prop = exp_table.consumed_R ./ total_consumed;
+total_licks = total_consumed * licks_per_ml;
+total_licks = round(total_licks + total_licks .* (rand(size(total_licks))-.5)/10);
 for r = 1:height(exp_table)
     if exp_table.lickometer(r) == 0
         continue
     end
-    noisy_rate = lick_rate + (randi(200)-100)/100/10/lick_rate; %add up to 10% noise
-    t =  generate_lick_times(noisy_rate, time_stop);
-
-    if exp_table.fluid_R{r} == "water"
-        right_prop = 0.5;
-    else
-        right_prop = ethanol_prop_baseline;
-        if exp_table.strain{r} == "Wistar"
-            right_prop = right_prop * wistar_reduction;
-        end
-        if exp_table.sex{r} == "F"
-            right_prop = right_prop * male_reduction;
-        end
-    end
-    is_right = model_bottle_switching(length(t), right_prop);
+    t =  generate_lick_times(total_licks(r), time_stop);
+    is_right = model_bottle_switching(length(t), r_prop(r));
     s = nan(size(is_right));
     s(is_right) = exp_table.sipper_R(r);
     s(~is_right) = exp_table.sipper_L(r);
@@ -65,10 +50,9 @@ lick_table = [lick_table; lick_table_off];
 lick_table = sortrows(lick_table,"timestamp","ascend");
 
 %%
-writetable(lick_table, 'randomly_generated.csv');
+writetable(lick_table, 'licks_20250000_000000.csv');
 
-function lick_times = generate_lick_times(lick_rate, time_stop)
-    num_licks = round(lick_rate * time_stop / 1000);
+function lick_times = generate_lick_times(num_licks, time_stop)
     chi_shape = randi(10);
     lick_times = random('Chisquare',chi_shape,[num_licks,1]);
     lick_times = sort(lick_times);
@@ -77,10 +61,9 @@ function lick_times = generate_lick_times(lick_rate, time_stop)
 end
 
 function is_right = model_bottle_switching(num_licks, right_prop)
-is_right = zeros(num_licks);
-is_right(1: round(num_licks*right_prop)) = 1;
-is_right = is_right(randperm(num_licks));
-lick_smoothing = 20;
-is_right = round(smooth(is_right, lick_smoothing));
-is_right = logical(is_right);
+clumpy_factor = 20;
+smooth_rand = smooth(randn([num_licks,1]), clumpy_factor);
+sorted = sort(smooth_rand);
+threshold = sorted(round(num_licks*right_prop));
+is_right = smooth_rand<threshold;
 end
