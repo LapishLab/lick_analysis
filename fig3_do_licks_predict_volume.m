acff586fff8b8ef2bb5cf7377bdee20b75cc28a4
David Swygart <davidswygart@gmail.com>
2025-07-01 17:52:57 -0400

    Update fig3_do_licks_predict_volume.m

--- fig3_do_licks_predict_volume.m
+++ fig3_do_licks_predict_volume.m
@@ -1,21 +1,41 @@
 clear
 load("all_days.mat")
- 
-%% obtain Licks and Volume consumed for alcohol
-exp_table = sortrows(exp_table,"consumed_R", "ascend");
-alc_volume = exp_table.consumed_R;
-alc_licks = exp_table.lickometer == 1;
- 
-%% obtain licks and volume consumed for water
-exp_table = sortrows(exp_table,"consumed_L","ascend");
-water_volume = exp_table.consumed_L;
+exp_table = exp_table(exp_table.lickometer==1, :);
+is_male = exp_table.sex=="M";
+exp_table.num_licks_R = cellfun(@height, exp_table.licks_R);
+exp_table.num_licks_L = cellfun(@height, exp_table.licks_L);
+%% Scatter licks vs. ethanol consumed
+f = figure(theme="light"); clf; hold on;
+f.Position = [100 100 400 400];
 
+x = exp_table.num_licks_R(is_male);
+y = exp_table.consumed_R(is_male);
+scatter(x, y , 'filled')
 
+x = exp_table.num_licks_R(~is_male);
+y = exp_table.consumed_R(~is_male);
+scatter(x, y , 'filled')
 
-%% david's quick and dirty
-figure(2); clf; hold on;
-scatter(cellfun(@height, exp_table.licks_R), exp_table.consumed_R , 'filled')
-scatter(cellfun(@height, exp_table.licks_L), exp_table.consumed_L , 'filled')
 xlabel('Total licks')
-ylabel('Total consumed (ml')
-legend('ethanol','water')
+ylabel('Volume consumed (ml)')
+title('Ethanol bottle')
+legend("Male", "Female", Location="northwest")
+exportgraphics(gca,['figures', filesep, 'f3_ethanol_licks.svg'])
+
+%% Scatter licks vs. ethanol consumed
+f = figure(theme="light"); clf; hold on;
+f.Position = [100 100 400 400];
+
+x = exp_table.num_licks_L(is_male);
+y = exp_table.consumed_L(is_male);
+scatter(x, y , 'filled')
+
+x = exp_table.num_licks_L(~is_male);
+y = exp_table.consumed_L(~is_male);
+scatter(x, y , 'filled')
+
+xlabel('Total licks')
+ylabel('Volume consumed (ml)')
+title('Water bottle')
+legend("Male", "Female", Location="northwest")
+exportgraphics(gca,['figures', filesep, 'f3_water_licks.svg'])
