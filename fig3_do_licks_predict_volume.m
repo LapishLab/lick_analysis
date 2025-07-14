410c9ae33e4306d6062968acfdf72639722e8df7
David Swygart <davidswygart@gmail.com>
2025-07-14 17:48:20 -0400

    fix switched fluid labels

--- fig3_do_licks_predict_volume.m
+++ fig3_do_licks_predict_volume.m
@@ -5,10 +5,10 @@ exp_table.num_licks = cellfun(@height, exp_table.licks);
 f = figure(theme="light"); clf; hold on;
 f.Position = [100 100 400 400];
 
-t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'water'},{'sex','M'});
+t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'ethanol'},{'sex','M'});
 scatter(t.num_licks, t.consumed, 'filled')
 
-t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'water'},{'sex','F'});
+t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'ethanol'},{'sex','F'});
 scatter(t.num_licks, t.consumed, 'filled')
 
 xlabel('Total licks')
@@ -21,10 +21,10 @@ exportgraphics(gca,['figures', filesep, 'f3_ethanol_licks.svg'])
 f = figure(theme="light"); clf; hold on;
 f.Position = [100 100 400 400];
 
-t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'ethanol'},{'sex','M'});
+t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'water'},{'sex','M'});
 scatter(t.num_licks, t.consumed, 'filled')
 
-t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'ethanol'},{'sex','F'});
+t = sub_table(exp_table, {'lickometer', 1},{'fluid', 'water'},{'sex','F'});
 scatter(t.num_licks, t.consumed, 'filled')
 
 xlabel('Total licks')
