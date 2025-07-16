382ef1643f6172b1c6a06b649a61de9ea8e278f3
David Swygart <davidswygart@gmail.com>
2025-07-16 14:09:37 -0400

    add changes to consumption figure

--- fig2_test_if_lickometer_changes_consumption.m
+++ fig2_test_if_lickometer_changes_consumption.m
@@ -1,71 +1,93 @@
 clear
 load("all_days.mat")
-fig_size = [400,400];
 
-%% get ethanol consumption
-eth = sub_table(exp_table, {'fluid', 'ethanol'});
-e_consumption = consumption_split_by_lickometer(eth);
-
-%% Plot bar graph of ethanol volume consumed
-f = figure(1); theme('light'); clf; hold on;
-f.Position = [80 80 fig_size];
-
-errbar_with_raw_data(e_consumption, ["No lick detector", "With lick detector"])
-ylabel('Volume consumed (ml)')
-title('10% Ethanol')
-exportgraphics(gca,['figures', filesep, 'f2_ethanol_consumption.svg'])
-
-% [~, p] = ttest(e_consumption(:,1), e_consumption(:,2));
+consumption = cell(2,1); % 1 = water (no lick, lick), 2 = ethanol (no lick, lick)
 
 %% get water consumption
 wat = sub_table(exp_table, {'fluid', 'water'});
 w_consumption = consumption_split_by_lickometer(wat);
+consumption{1} = w_consumption; 
 
-%% Plot bar graph of water volume consumed
-f = figure(2); theme('light'); clf; hold on;
-f.Position = [480 80 fig_size];
+%% get ethanol consumption
+eth = sub_table(exp_table, {'fluid', 'ethanol'});
+e_consumption = consumption_split_by_lickometer(eth);
+consumption{2} = e_consumption;
+%% Plot bar graph of ethanol volume consumed
+f = figure(1); theme('light'); clf; hold on;
+fig_size = [500,400];
+f.Position = [80 80 fig_size];
 
-errbar_with_raw_data(w_consumption, ["No lick detector", "With lick detector"])
+b = errbar_with_raw_data(consumption, ["Water", "10% ethanol"]);
+
+water_color = [35, 37, 150] / 255;
+eth_color = [4, 64, 15] / 255;
+lightening = 2.5;
+b(1).CData(1,:) =  water_color * lightening; %no_lick_water
+b(1).CData(2,:)  = eth_color * lightening; % no_lick_eth
+b(2).CData(1,:) = water_color; %  lick_water
+b(2).CData(2,:) = eth_color; %lick_eth 
+
 ylabel('Volume consumed (ml)')
-title('Water')
-exportgraphics(gca,['figures', filesep, 'f2_water_consumption.svg'])
 
-% [~, p] = ttest(w_consumption(:,1), w_consumption(:,2))
+
+exportgraphics(gca,['figures', filesep, 'f2_ethanol_consumption.svg'])
+
+% [~, p] = ttest(e_consumption(:,1), e_consumption(:,2));
+
+
 
 %% Plot bar graph of consumption percentage with lickometer
-f = figure(3); theme('light'); clf; hold on;
-f.Position = [880 80 fig_size];
+f = figure(2); theme('light'); clf; hold on;
+fig_size = [300,400];
+f.Position = [480 80 fig_size];
 difference = table();
 difference.water = (w_consumption.lick - w_consumption.no_lick);
 difference.ethanol = (e_consumption.lick - e_consumption.no_lick);
-errbar_with_raw_data(difference, ["Water", "10% Ethanol"])
+b = errbar_with_raw_data2({difference}, ["Water", "10% Ethanol"]);
+
+b(1).CData(1,:) =  water_color * lightening/2; %no_lick_water
+b(1).CData(2,:)  = eth_color * lightening/2; % no_lick_eth
+
 ylabel('Consumption difference with lick detector (ml)')
-title('Consumption change per rat')
-ylim([min(difference{:,:}, [], 'all'), max(difference{:,:}, [], 'all')])
+
+ylim(ylim*1.1)
 exportgraphics(gca,['figures', filesep, 'f2_diff_consumption.svg'])
 
 
 %%
 
-function errbar_with_raw_data(data, labels)
+function b = errbar_with_raw_data(data, labels)
 % data = table or matrix with each column being different dataset
 % string labels for each group (x-axis of bar graph)
-data = table2array(data);
-avg = mean(data);
-err = sem(data);
 
-bar(labels, avg)
-errorbar(avg,err, 'k', 'LineStyle', 'none', 'CapSize',50,'LineWidth',2)
+avg = nan(length(data), width(data{1}));
+err = avg;
 
-x = 1:size(data,2);
-x = repmat(x,size(data,1),1);
+for i=1:length(data)
+    d = table2array(data{i});
+    avg(i,:) = mean(d);
+    err(i,:) = sem(d);
+end
+b = bar(labels, avg, 'FaceColor','flat');
 
-jiggle_scale = .1;
-x_jiggle = jiggle_scale*(randn(size(x)));
-x = x + x_jiggle;
-scatter(x,data, 'filled', 'k')
-ylim([0, max(data(:))*1.1])
+x = cell2mat({b.XEndPoints}');
+avg = avg';
+err = err';
 
+errorbar(x(:), avg(:), err(:), 'k', 'LineStyle', 'none', 'CapSize',20,'LineWidth',2)
+
+jiggle_scale = .02;
+for r = 1:size(x,1)
+    for c = 1:size(x,2)
+        d = data{c}{:,r};
+        bar_x = repmat(x(r,c), length(d), 1);
+        bar_x = bar_x + jiggle_scale*(randn(size(bar_x)));
+
+        scatter(bar_x,d, 'filled', 'k')
+    end
+end
+
+% ylim([0, max(data(:))*1.1])
 end
 
 function vol = consumption_per_rat(rat_ids, exp)
@@ -93,3 +115,33 @@ function consumption = consumption_split_by_lickometer(exp)
     consumption = rmmissing(consumption);
 end
 
+function b = errbar_with_raw_data2(data, labels)
+% data = table or matrix with each column being different dataset
+% string labels for each group (x-axis of bar graph)
+
+avg = nan(length(data), width(data{1}));
+err = avg;
+
+for i=1:length(data)
+    d = table2array(data{i});
+    avg(i,:) = mean(d);
+    err(i,:) = sem(d);
+end
+b = bar(labels, avg, 'FaceColor','flat');
+
+x = cell2mat({b.XEndPoints}');
+avg = avg';
+err = err';
+
+errorbar(x(:), avg(:), err(:), 'k', 'LineStyle', 'none', 'CapSize',20,'LineWidth',2)
+
+jiggle_scale = .1;
+
+for c = 1:size(x,2)
+    d = data{1}{:,c};
+    bar_x = repmat(x(c), length(d), 1);
+    bar_x = bar_x + jiggle_scale*(randn(size(bar_x)));
+
+    scatter(bar_x,d, 'filled', 'k')
+end
+end
